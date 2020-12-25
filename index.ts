b95b97c3a58294fc6084909395ccb2d0e95948f6
RobinLoskli <ami0504@mail.ru>
2020-12-26 00:11:37 +1000

    fix

--- index.ts
+++ index.ts
@@ -2,6 +2,6 @@ import * as fs     from 'fs';
 import * as parser from './parser';
 
 let fileData: string = fs.readFileSync('data.txt','utf-8');
+fs.writeFileSync('output.txt', parser.languageParser.parse(fileData).result);
 console.log('code was compiled successfully');
-fs.writeFileSync('output.txt', parser.languageParser.parse(fileData).result)
 
