8ded0cbfb54ebecad3385204430db2619fcfbf88
RobinLoskli <ami0504@mail.ru>
2020-12-26 00:12:15 +1000

    fix

--- libs/ErrorModel.ts
+++ libs/ErrorModel.ts
@@ -1,6 +1,6 @@
 class ErrorModel{
     public term   : string;
-    public input : string;
+    public input  : string;
     public message: string;
 
     constructor(mes_: string, term_: string, input_: string){
